33e670ebb24972070aba018d001cac7933528e8d
github username <github email address>
2018-11-07 14:40:43 -0500

    attempting to connect frontend to heroku

--- soundscribe_backend/config/routes.rb
+++ soundscribe_backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :songs, :notes, :melodies
+
+  get '/', to: 'songs#index'
 end
